d7fc9afb6590aafa0e3fa812a75a6190d937e2fc
Alekos Filini <alekos.filini@gmail.com>
2020-05-09 00:26:05 +0200

    Various playground improvements, add the compiler

--- playground/src/lib.rs
+++ playground/src/lib.rs
@@ -1,18 +1,28 @@
+use std::collections::HashMap;
 use std::rc::Rc;
+use std::str::FromStr;
 
+use js_sys::Promise;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::future_to_promise;
-use js_sys::Promise;
 
-use log::{info, debug};
+use rand::Rng;
+
+use log::{debug, info};
+
+use serde::Deserialize;
 
 use clap::AppSettings;
 
-use magical_bitcoin_wallet::*;
+use magical_bitcoin_wallet::bitcoin;
 use magical_bitcoin_wallet::database::memory::MemoryDatabase;
-use magical_bitcoin_wallet::bitcoin as bitcoin;
+use magical_bitcoin_wallet::miniscript;
+use magical_bitcoin_wallet::*;
 
 use bitcoin::*;
+
+use miniscript::policy::Concrete;
+use miniscript::Descriptor;
 
 mod utils;
 
@@ -39,7 +49,12 @@ pub struct WalletWrapper {
 #[wasm_bindgen]
 impl WalletWrapper {
     #[wasm_bindgen(constructor)]
-    pub async fn new(network: String, descriptor: String, change_descriptor: Option<String>, esplora: String) -> Result<WalletWrapper, String> {
+    pub async fn new(
+        network: String,
+        descriptor: String,
+        change_descriptor: Option<String>,
+        esplora: String,
+    ) -> Result<WalletWrapper, String> {
         let network = match network.as_str() {
             "regtest" => Network::Regtest,
             "testnet" | _ => Network::Testnet,
@@ -48,9 +63,17 @@ impl WalletWrapper {
         debug!("descriptors: {:?} {:?}", descriptor, change_descriptor);
 
         let blockchain = EsploraBlockchain::new(&esplora);
-        let wallet = Wallet::new(descriptor.as_str(), change_descriptor.as_ref().map(|x| x.as_str()), network, MemoryDatabase::new(), blockchain).await.map_err(|e| format!("{:?}", e))?;
+        let wallet = Wallet::new(
+            descriptor.as_str(),
+            change_descriptor.as_ref().map(|x| x.as_str()),
+            network,
+            MemoryDatabase::new(),
+            blockchain,
+        )
+        .await
+        .map_err(|e| format!("{:?}", e))?;
 
-        Ok(WalletWrapper{
+        Ok(WalletWrapper {
             _change_descriptor: change_descriptor,
             wallet: Rc::new(wallet),
         })
@@ -62,11 +85,78 @@ impl WalletWrapper {
         let wallet = Rc::clone(&self.wallet);
 
         future_to_promise(async move {
-            let matches = app.get_matches_from_safe_borrow(line.split(" ")).map_err(|e| e.message)?;
-            let res = cli::handle_matches(&wallet, matches).await.map_err(|e| format!("{:?}", e))?;
+            let matches = app
+                .get_matches_from_safe_borrow(line.split(" "))
+                .map_err(|e| e.message)?;
+            let res = cli::handle_matches(&wallet, matches)
+                .await
+                .map_err(|e| format!("{:?}", e))?;
 
             Ok(res.into())
         })
     }
 }
 
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum Alias {
+    GenWif,
+    GenExt { extra: String },
+    Existing { extra: String },
+}
+
+impl Alias {
+    fn into_key(self) -> String {
+        match self {
+            Alias::GenWif => {
+                let key = secp256k1::key::SecretKey::new(&mut rand::thread_rng());
+                let sk = bitcoin::util::key::PrivateKey {
+                    compressed: true,
+                    network: Network::Testnet,
+                    key,
+                };
+
+                sk.to_wif()
+            }
+            Alias::GenExt { extra: path } => {
+                let seed = rand::thread_rng().gen::<[u8; 32]>();
+                let xprv =
+                    bitcoin::util::bip32::ExtendedPrivKey::new_master(Network::Testnet, &seed)
+                        .unwrap();
+
+                format!("{}{}", xprv, path)
+            }
+            Alias::Existing { extra } => extra,
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn compile(policy: String, aliases: String, script_type: String) -> Promise {
+    future_to_promise(async move {
+        let aliases: HashMap<String, Alias> =
+            serde_json::from_str(&aliases).map_err(|e| format!("{:?}", e))?;
+        let aliases: HashMap<String, String> = aliases
+            .into_iter()
+            .map(|(k, v)| (k, v.into_key()))
+            .collect();
+
+        let policy = Concrete::<String>::from_str(&policy).map_err(|e| format!("{:?}", e))?;
+        let compiled = policy.compile().map_err(|e| format!("{:?}", e))?;
+
+        let descriptor = match script_type.as_str() {
+            "sh" => Descriptor::Sh(compiled),
+            "wsh" => Descriptor::Wsh(compiled),
+            "sh-wsh" => Descriptor::ShWsh(compiled),
+            _ => return Err("InvalidScriptType".into()),
+        };
+
+        let descriptor: Result<Descriptor<String>, String> = descriptor.translate_pk(
+            |key| Ok(aliases.get(key).unwrap_or(key).into()),
+            |key| Ok(aliases.get(key).unwrap_or(key).into()),
+        );
+        let descriptor = descriptor?;
+
+        Ok(format!("{}", descriptor).into())
+    })
+}
--- playground/www/src/index.js
+++ playground/www/src/index.js
@@ -1,4 +1,4 @@
-import { WalletWrapper, init } from "wasm-esplora";
+import { WalletWrapper, init, compile } from "magical-bitcoin-wallet-playground";
 
 async function startWallet(desc, change_desc) {
     const stdout = document.getElementById("stdout");
@@ -70,12 +70,16 @@ async function startWallet(desc, change_desc) {
 (async function() {
     init();
 
+    let currentWallet = null;
+
     document.getElementById("stdin").disabled = true;
 
     const start_button = document.getElementById("start_button");
+    const stop_button = document.getElementById("stop_button");
     const start_message = document.getElementById("start_message");
 
     start_button.disabled = false;
+    stop_button.disabled = true;
 
     const descriptor = document.getElementById("descriptor"); 
     const change_descriptor = document.getElementById("change_descriptor"); 
@@ -90,13 +94,75 @@ async function startWallet(desc, change_desc) {
         startWallet(descriptor.value, change_descriptor.value.length > 0 ? change_descriptor.value : null)
             .then((w) => {
                 start_button.disabled = true;
+
                 descriptor.disabled = true;
                 change_descriptor.disabled = true;
 
                 start_message.innerHTML = "Wallet created, running `sync`...";
 
                 w.run('sync').then(() => start_message.innerHTML = "Ready!");
+
+                currentWallet = w;
+                stop_button.disabled = false;
             })
             .catch((err) => start_message.innerHTML = `<span class="error">${err}</span>`);
     };
+
+    stop_button.onclick = (e) => {
+        if (currentWallet == null) {
+            return;
+        }
+
+        e.preventDefault();
+
+        currentWallet.free();
+        start_message.innerHTML = "Wallet instance destroyed";
+
+        start_button.disabled = false;
+        stop_button.disabled = true;
+
+        descriptor.disabled = false;
+        change_descriptor.disabled = false;
+    };
+
+    const policy = document.getElementById('policy');
+    const compiler_script_type = document.getElementById('compiler_script_type');
+    const compiler_output = document.getElementById('compiler_output');
+    const compile_button = document.getElementById('compile_button');
+    compile_button.onclick = (e) => {
+        if (policy.value.length == 0) {
+            return;
+        }
+
+        e.preventDefault();
+
+        const single = !e.target.form.elements.namedItem('alias').length;
+
+        let elements_alias = e.target.form.elements.namedItem('alias');
+        let elements_type = e.target.form.elements.namedItem('type');
+        let elements_extra = e.target.form.elements.namedItem('extra');
+
+        if (single) {
+            elements_alias = [elements_alias];
+            elements_type = [elements_type];
+            elements_extra = [elements_extra];
+        } else {
+            elements_alias = Array.from(elements_alias);
+            elements_type = Array.from(elements_type);
+            elements_extra = Array.from(elements_extra);
+        }
+
+        const aliases = {};
+        elements_alias.forEach((alias) => {
+            const type = elements_type.filter((x) => x.attributes["data-index"].value == alias.attributes["data-index"].value)[0].value;
+            const extra = elements_extra.filter((x) => x.attributes["data-index"].value == alias.attributes["data-index"].value)[0].value;
+            const aliasValue = alias.value;
+
+            aliases[aliasValue] = { type, extra };
+        });
+
+        compile(policy.value, JSON.stringify(aliases), compiler_script_type.value)
+            .then(res => compiler_output.innerHTML = res)
+            .catch(err => compiler_output.innerHTML = `<span class="error">${err}</span>`);
+    }
 })();
