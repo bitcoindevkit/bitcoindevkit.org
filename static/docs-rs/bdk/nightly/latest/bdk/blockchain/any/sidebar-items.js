75406910dcc06c39ba402d9d2610ea999e1aadca
Steve Myers <steve@notmandatory.org>
2021-12-17 21:56:37 -0800

    Remove the ./static directory
    The files are not needed since migrating from Hugo to Vuepress. The
    docs-rs/bdk/nightly files are now stored in ./docs/.viewpress/public

--- static/docs-rs/bdk/nightly/latest/bdk/blockchain/any/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["AnyBlockchain","Type that can contain any of the [`Blockchain`] types defined by the library"],["AnyBlockchainConfig","Type that can contain any of the blockchain configurations defined by the library"]]});
