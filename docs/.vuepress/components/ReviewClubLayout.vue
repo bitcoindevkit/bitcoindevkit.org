1be7febfdee589cc5681958a74bb72e41498fa27
Vladimir Fomene <vladimirfomene@gmail.com>
2023-06-07 10:00:51 +0300

    Add layout for ReviewClub pages

---
+++ docs/.vuepress/components/ReviewClubLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <div class="header-container">
+      <ReviewClubHeader />
+    </div>
+    <div class="body-container">
+      <Content/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ReviewClubHeader from "./ReviewClubHeader.vue";
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    ReviewClubHeader
+  }
+}
+</script>
+
+<style>
+.body-container {
+  color: #000;
+  box-sizing: border-box;
+  display: flex;
+  margin-left: 10%;
+  margin-right: 10%;
+  font-family: "Helvetica",sans-serif;
+  line-height: 1.5;
+}
+
+.header-container {
+  color: #000;
+  box-sizing: border-box;
+  display: flex;
+  margin-left: 10%;
+  margin-right: 10%;
+  font-family: "Helvetica",sans-serif;
+  line-height: 1.5;
+}
+
+.body-container .question {
+  font-weight: bold;
+  
+}
+
+.body-container a:link {
+  color: #0000f0;
+}
+
+.body-container a:visited {
+  color: #551b8c;
+}
+
+.body-container a {
+  text-decoration: underline;
+}
+</style>
