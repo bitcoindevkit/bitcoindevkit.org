0c1823bbf8fb96451f8d9e8b06426cd0965a9c57
Vladimir Fomene <vladimirfomene@gmail.com>
2023-06-13 13:49:39 +0300

    Add header layout for review club site

---
+++ docs/.vuepress/components/ReviewClubHeader.vue
@@ -0,0 +1,58 @@
+<template>
+    <header>
+        <div class="logo">
+            <a href="/review-club">BitcoinDevKit PR Review Club</a>
+        </div>
+        <nav>
+            |<span v-for="nav in navs">
+            <a :href="nav.link" class="nav-link">
+                {{ nav.name }}
+            </a> |</span>
+        </nav>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: "ReviewClubHeader",
+    data() {
+        return {
+            navs: [{name: "ReviewClub Home", link: "/review-club/"}, {name: "BitcoinDevKit Home", link: "/"}, {name: "Meetings", link: "/review-club/meetings/"}, {name: "Code of Conduct", link: "/review-club/code-of-conduct/"}, {name: "Hosting", link: "/review-club/hosting/"}],
+        }
+    }
+}
+</script>
+
+<style>
+header {
+    font-family: "Helvetica",sans-serif;
+    line-height: 1.5;
+}
+
+.logo {
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 48px;
+}
+
+.logo a:link, .logo a:visited {
+    color: #000;
+
+}
+
+a.nav-link {
+    display: inline-block;
+    margin-left: 5px;
+    color: #0000f0;
+    text-decoration: underline;
+}
+
+a.nav-link:visited {
+    color: #551b8c;
+}
+
+a.nav-link:hover {
+    color: #fcc603;
+}
+</style>
